5e9d38103a07e5a23a3a3d303985490c5ee345bd
v66v <74927439+v66v@users.noreply.github.com>
2024-06-28 03:12:04 +0300

    upd: rename plotutils to glotputils

--- guix.scm
+++ guix.scm
@@ -1,13 +1,9 @@
 (use-modules (guix)
              (guix build-system gnu)
              (guix git-download)
-             ;; (gnu packages pkg-config)
-             ;; (gnu packages gcc)
-             ;; (gnu packages guile)
+             (gnu packages)
              (gnu packages commencement)
              (gnu packages autotools)
-             ;; (gnu packages base)
-             (gnu packages)
 			 (gnu packages xorg)
 			 (gnu packages image)
              ((guix licenses)
@@ -18,10 +14,10 @@
   (or (git-predicate (current-source-directory))
       (const #t)))
 
-(define plotutils
+(define glotputils
   (package
-    (name "plotutils")
-    (version "2.6")
+    (name "glotputils")
+    (version "0.1")
     (source (origin
               (method url-fetch)
               (uri (string-append "file://" (current-source-directory)))
@@ -29,32 +25,15 @@
                (base32
                 "1arkyizn5wbgvbh53aziv3s6lmd3wm9lqzkhxb3hijlp1y124hjg"))
               (modules '((guix build utils)))
-              (snippet
-               ;; Force the use of libXaw7 instead of libXaw.  When not doing
-               ;; that, libplot.la ends up containing just "-lXaw" (without
-               ;; "-L/path/to/Xaw"), due to the fact that there is no
-               ;; libXaw.la, which forces us to propagate libXaw.
-               '(begin
-                  (substitute* "configure"
-                    (("-lXaw")
-                     "-lXaw7"))
-                  ;; Use the `png_jmpbuf' accessor, as recommended since libpng
-                  ;; 1.4.0 (see:
-                  ;; http://www.libpng.org/pub/png/src/libpng-1.2.x-to-1.4.x-summary.txt).
-                  (substitute* "libplot/z_write.c"
-                    (("png_ptr->jmpbuf")
-                     "png_jmpbuf (png_ptr)"))
-                  #t))
               (patches
                ;; The test suite fails on some architectures such as i686 (see:
-               ;; https://lists.gnu.org/archive/html/bug-plotutils/2016-04/msg00002.html).
+               ;; https://lists.gnu.org/archive/html/bug-glotputils/2016-04/msg00002.html).
                ;; The following Debian patch works around it.
-               (search-patches "plotutils-spline-test.patch"))))
+               (search-patches "glotputils-spline-test.patch"))))
     (build-system gnu-build-system)
     (arguments
      (list #:configure-flags
            #~(list "--enable-libplotter"
-
                    ;; On i686 some tests fail due to excess floating point
                    ;; precision; work around it.  However, libplotter is C++
                    ;; and thus unaffected by CFLAGS, but '-fexcess-precision'
@@ -66,22 +45,22 @@
            #:phases
            (if (target-x86-32?)
                #~(modify-phases %standard-phases
-                   (add-before 'check 'skip-sloppy-test
-                     (lambda _
-                       ;; This test reveals a slight difference in the SVG
-                       ;; output due to floating point inequalities.  Skip it.
-                       (substitute* "test/plot2svg.test"
-                         (("^exit .*") "exit 77")))))
+								(add-before 'check 'skip-sloppy-test
+											(lambda _
+											  ;; This test reveals a slight difference in the SVG
+											  ;; output due to floating point inequalities.  Skip it.
+											  (substitute* "test/plot2svg.test"
+														   (("^exit .*") "exit 77")))))
                #~%standard-phases)))
     (inputs (list libpng libx11 libxt libxaw gcc-toolchain automake autoconf libtool))
-    (home-page "https://www.gnu.org/software/plotutils/")
+    (home-page "https://github.com/v66v/glotputils")
     (synopsis "Plotting utilities and library")
     (description
-     "GNU Plotutils is a package for plotting and working with 2D graphics.
+     "Glotputils is a package for plotting and working with 2D graphics.
 It includes the C library @code{libplot} and the C++ @code{libplotter} library
 for exporting 2D vector graphics in many file formats.  It also has support
 for 2D vector graphics animations.  The package also contains command-line
 programs for plotting scientific data.")
     (license license:gpl2+)))
 
-plotutils
+glotputils
