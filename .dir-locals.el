b16fd02ef9b046da4076d857546046763197e610
v66v <74927439+v66v@users.noreply.github.com>
2024-06-30 00:20:27 +0300

    update build system

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((compile-command . "guix shell --pure -Df guix.scm -- make -j4"))))
+((nil . ((compile-command . "guix shell --pure -Df guix.scm -- make -j4 -C graph"))))
